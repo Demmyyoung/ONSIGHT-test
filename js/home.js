fe65fc6ebcd00fbe700dc9f9a49e21ef9f6ce87d
Demmyyoung <112753184+Demmyyoung@users.noreply.github.com>
2024-03-03 09:28:20 +0100

    Updatev1.1
    Added buy feature and fixed some bugs

--- js/home.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu() {
-  var menuToggle = document.querySelector('.menu-toggle');
-  menuTogglefunction toggleMenu() {
-  var menuToggle = document.querySelector('.menu-toggle');
-  menuToggle
